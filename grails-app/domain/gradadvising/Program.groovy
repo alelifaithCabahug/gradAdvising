0abea3a6fd1d3b272e6b9c005b62c3b51570240d
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2013-07-18 15:46:01 +0800

    Program Domain

--- grails-app/domain/gradadvising/Program.groovy
+++ grails-app/domain/gradadvising/Program.groovy
@@ -2,6 +2,17 @@ package gradadvising
 
 class Program {
 
+    String programName
+	
+	static hasMany = [subject:Subject, student:Student]
+	
     static constraints = {
+		programName(blank:false, unique:true)
     }
+	
+
+	String toString(){
+		"${this.programName}"
+	}
+	
 }
