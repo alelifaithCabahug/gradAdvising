d47f63365d7bbe46517c2786e5947b81aba0c86f
elisamay <metotcom@gmail.com>
2013-07-24 22:52:05 +0800

    Update codes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,6 +42,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
 
         // runtime 'mysql:mysql-connector-java:5.1.22'
+		runtime 'postgresql:postgresql:9.1-902.jdbc4'
     }
 
     plugins {
@@ -56,7 +57,6 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
     }
