872923674134d973403f2ce71d459187a6a30dac
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2013-08-01 23:55:00 +0800

    added numberOfYears for the program

--- grails-app/domain/gradadvising/Program.groovy
+++ grails-app/domain/gradadvising/Program.groovy
@@ -3,11 +3,13 @@ package gradadvising
 class Program {
 
     String programName
+	int numberOfYears
 	
 	static hasMany = [subject:Subject, student:Student]
 	
     static constraints = {
 		programName(blank:false, unique:true)
+		numberOfYears(blank:false)
     }
 	
 	static searchable = true
