833ac2d858e30503c3d0d91a098716d110a8428b
aum_daganga <lyrexzalyzza@gmail.com>
2013-08-05 16:33:35 +0800

    edited YearSem Domain
    initial domain for Year & Semester

--- grails-app/domain/gradadvising/YearSem.groovy
+++ grails-app/domain/gradadvising/YearSem.groovy
@@ -1,32 +1,12 @@
 package gradadvising
 
 class YearSem {
-	int year
-	int sem
+	String year
+	String sem
 	
-	public YearSem(int y, int s)
-	{
-		year = y
-		sem = s
-	}
+	 static constraints = {
+		year(blank:false, unique:true)
+		sem(blank:false)
+    }
 	
-	public String toString()
-	{
-	    StringBuffer s = new StringBuffer(Integer.toString(year) + "-" +Integer.toString(year+1));
-		switch(sem)
-		{
-			case 1: 
-			   s.append(" Sem 1")
-			   break
-			case 2:
-			   s.append(" Sem 2")
-			   break
-			case 3:
-			   s.append(" Summer")
-			   break;
-			   
-		}
-		
-		return s.toString();
-	}
 }
