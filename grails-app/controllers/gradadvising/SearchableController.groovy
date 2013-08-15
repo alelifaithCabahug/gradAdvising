30fbb81322a52888b44714efce66cb5bff56e38c
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2013-08-15 08:46:39 +0800

    updated SearchableController

--- grails-app/controllers/gradadvising/SearchableController.groovy
+++ grails-app/controllers/gradadvising/SearchableController.groovy
@@ -1,6 +1,23 @@
 package gradadvising
 
-class SearchableController {
+import org.compass.core.engine.SearchEngineQueryParseException
 
-    def index() { }
+/*
+*	This controller is taken from the searchable plugin
+*/
+
+class SearchableController {
+    def searchableService
+
+    def index = {
+        if (!params.q?.trim()) {
+            return [:]
+        }
+        try {
+            return [searchResult: searchableService.search(params.q, params)]
+        } catch (SearchEngineQueryParseException ex) {
+            return [parseException: true]
+        }
+    }
+
 }
--- grails-app/domain/gradadvising/Enrollments.groovy
+++ grails-app/domain/gradadvising/Enrollments.groovy
@@ -17,3 +17,4 @@ class Enrollments {
 	}
 	
 	//getGPA to be implemented
+}
