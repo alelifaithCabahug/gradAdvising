5fcc5cf28e6d4c23112f1e71bfb0bcb461ecee15
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2013-09-02 14:58:06 +0800

    username and password attributes to student domain

--- grails-app/domain/gradadvising/Student.groovy
+++ grails-app/domain/gradadvising/Student.groovy
@@ -12,6 +12,10 @@ class Student{
 	boolean withSuitableBackground
 	int yearLevel
 	
+	//attributes for login
+	String username
+	String password
+	
 	static belongsTo = [Program]
 	
 	static hasMany = [enrollments:Enrollments]
@@ -27,6 +31,8 @@ class Student{
 		dateEnrolled()
 		withGPAof2Above()
 		withSuitableBackground()
+		username(unique:true, blank:false)
+		password(blank:false)
     }
 	
 	static searchable = true
