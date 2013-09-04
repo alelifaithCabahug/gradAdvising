2f75e7a7b3b9e44d1b0c8b675a85fe3bbf566336
aum_daganga <lyrexzalyzza@gmail.com>
2013-09-04 21:53:14 +0800

    Set YearSem domain min and max values

--- grails-app/domain/gradadvising/YearSem.groovy
+++ grails-app/domain/gradadvising/YearSem.groovy
@@ -1,20 +1,37 @@
 package gradadvising
 
 class YearSem {
-	String year
-	String sem
+	int year
+	int sem
 	
-	 static constraints = {
-		year(blank:false, unique:true)
-		sem(inList: ["1ST Semester","2ND Semester","Summer"])
+	static constraints = {
+		year(blank:false, min:2000, max:2050)
+		sem(blank:false, min:1, max:3)
     }
 	
-	String toString(){
-<<<<<<< HEAD
-		"{this.year} ${this.sem}"
-=======
-	"${this.year} ${sem}"
->>>>>>> d2739500b215ec5232e00c397bd868a2f06d3b03
+	public YearSem(int y, int s)
+	{
+		year = y
+		sem = s
 	}
 	
+	public String toString()
+	{
+	    StringBuffer s = new StringBuffer(Integer.toString(year) + "-" +Integer.toString(year+1));
+		switch(sem)
+		{
+			case 1: 
+			   s.append(" Sem 1")
+			   break
+			case 2:
+			   s.append(" Sem 2")
+			   break
+			case 3:
+			   s.append(" Summer")
+			   break;
+			   
+		}
+		
+		return s.toString();
+	}
 }
