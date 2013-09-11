c224f2dff1c572ed16d27e347db65b088b0e8032
alelifaithCabahug <alelicabahug@gmail.com>
2013-09-11 13:16:19 +0800

    Edited BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,14 @@
+import gradadvising.Adviser
 class BootStrap {
+		def init = { servletContext ->
+			final String BACKUP_ADMIN = 'admin123'
+				if (!Adviser.findByUsername(BACKUP_ADMIN)) {
+					new Adviser(username:BACKUP_ADMIN,password:'password').save()
+				}
+			}
+		def destroy = {
+	}
+}class BootStrap {
 
     def init = { servletContext ->
     }
