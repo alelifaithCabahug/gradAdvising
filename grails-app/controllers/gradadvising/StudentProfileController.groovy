29e8ae8821ec9045363e3fac307d7c515f9559b1
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2013-09-11 20:38:39 +0800

    student profile controller

---
+++ grails-app/controllers/gradadvising/StudentProfileController.groovy
@@ -0,0 +1,6 @@
+package gradadvising
+
+class StudentProfileController {
+
+    def index() { }
+}
---
+++ test/unit/gradadvising/StudentProfileControllerTests.groovy
@@ -0,0 +1,17 @@
+package gradadvising
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(StudentProfileController)
+class StudentProfileControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
