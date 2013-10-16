46fc38308ef95738d26368f6a97ba49976be4706
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2013-10-16 15:00:02 +0800

    cor jasper and jrxml files

--- grails-app/controllers/gradadvising/ReportController.groovy
+++ grails-app/controllers/gradadvising/ReportController.groovy
@@ -26,6 +26,12 @@ class ReportController {
 		def enrollmentsInstance = Enrollments.get(params.enrollmentsId)
 		def db = new Sql(dataSource)
 		def result = db.rows("SELECT student.under_grad_course,student.bachelors_degree,student.id_number,student.date_enrolled,student.student_name,student.year_level, program.program_name,  subject.subject_code, subject.subject_description, subject.unit_load, enrollments.yr_sem_string FROM student INNER JOIN program on student.program_id = program.id INNER JOIN enrollments on student.id = enrollments.student_id INNER JOIN year_sem on enrollments.yrsem_id = year_sem.id INNER JOIN enrollments_subject on enrollments.id = enrollments_subject.enrollments_subject_id INNER JOIN subject on enrollments_subject.subject_id = subject.id where student.id = '${studentInstance.id}' and enrollments.id = '${enrollmentsInstance.id}' order by subject.id")
+		
+		def totalUnits = 0;
+		enrollmentsInstance.subject.each(){
+			totalUnits = totalUnits + it.unitLoad
+		}
+		params.total_units = totalUnits
 		chain(controller:'jasper',action:'index',model:[data:result],params:params)
 	}
 }
